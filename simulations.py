e082bb0ed4f834e1523767a3b25a59f08e63bc0a
Physix <159613996+1physix@users.noreply.github.com>
2025-04-13 22:00:21 +0000

    Simulations Day 1

--- simulations.py
+++ simulations.py
@@ -1,1 +1,15 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
 
+fig, ax = plt.subplots()
+x = np.linspace(0, 2 * np.pi, 200)
+line, = ax.plot(x, np.sin(x))
+
+def update(frame):
+    line.set_ydata(np.sin(x + frame / 10))  # Update y-data
+    return line,
+
+ani = animation.FuncAnimation(fig, update, frames=100, interval=50, blit=True)
+
+plt.show()
